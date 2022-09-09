691a60b9e0fcf0c3225fe7878c408ffb324026c3
Hugge25 <hugo.paulsson@hotmail.com>
2022-09-09 21:04:14 +0200

    Add project files.

---
+++ kap2.uppgfit2.13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgfit_2._13
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vad är Eriks lön?");
+            string Erik = Console.ReadLine();
+            Console.WriteLine("Vad är Marias lön?");
+            string Maria = Console.ReadLine();
+            Console.WriteLine("Vad är Jimmies lön?");
+            string Jimmie = Console.ReadLine();
+
+            int ErikLön = int.Parse(Erik);
+            int MariaLön = int.Parse(Maria);
+            int JimmieLön = int.Parse(Jimmie);
+
+            int Medellön = (ErikLön + MariaLön + JimmieLön) / 3;
+
+            Console.WriteLine("Medellönen för personalen är " + Medellön);
+        }
+    }
+}
